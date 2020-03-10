73a331e82dc148f834d39c875617829be16e6094
Aluno Lab. 03 <aluno03@aeelabs.edu.br>
2020-03-09 21:36:09 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,79 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import PlayerDetail from './PlayerDetail';
+import 'bootstrap/dist/css/bootstrap.css'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+class App extends  React.Component {  
+      
+      VetorJogadores = [
+            {
+                  nome : 'Allejo',
+                  pais : 'Brasil',
+                  time : ' Deportivo La Coruña'
+                  },
+            {
+                  nome : 'Gomez',
+                  pais : 'Brasil',
+                  time : 'Barcelona'
+                  },
+            {
+                  nome : 'Galfano',
+                  pais : 'Italia',
+                  time : 'Juventus'
+                  }
+      ]
+
+
+      constructor(props){
+        
+            super(props);
+
+            console.log(this);
+
+            this.state = {
+                  indice: 0
+            }
+
+      }
+
+      incrementarIndice = (event) => {
+        if (this.state.indice<=this.VetorJogadores.length-2)
+        {
+            this.setState({
+                  indice: this.state.indice+1
+            })
+        }
+      }
+
+      decrementarIndice = (event) => {
+            
+        if (this.state.indice!==0) {
+            this.setState({
+                      indice: this.state.indice-1  
+            })
+        }
+        
+      }
+      
+      render(){
+             return(
+                <div className="container">
+
+                <br/>
+                <PlayerDetail nome="Eliseu Dias Rodrigues" VetorJogadores={this.VetorJogadores[this.state.indice]}>
+                </PlayerDetail>
+                <br/>
+
+                        <button type="button" className ="btn btn-primary" onClick = {this.decrementarIndice} >Anterior</button>
+                            
+                        <button type="button" className ="btn btn-primary"  onClick = {this.incrementarIndice} >Proximo</button>
+
+                   </div>    
+            );      
+      }
+}
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
